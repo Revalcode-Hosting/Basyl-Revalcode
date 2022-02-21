0102ea147964da19c2fd7ac7d21e2d7cd7207a9a
Sophiae <sophia.makita@revalcodesolution.comm>
2022-02-21 17:39:08 -0300

    dropdown menu

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,13 +2,14 @@
 function AboutUs() {
     return (
       <div id='aboutus'>
+                <img className='aboutImgTwo small' alt="about us" src={require('../resources/aboutus.png')}/>
           <div className="row aboutUs">
-            <div className="col">
-                <img className='aboutImg' alt="about us" src={require('../resources/aboutus.png')}/>
+            <div className="col disappear">
+                <img className='aboutImg big' alt="about us" src={require('../resources/aboutus.png')}/>
             </div>
             
             <div className="col">
-                <h2>AboutUs</h2>
+                <h2 className='aboutT'>AboutUs</h2>
                 <p>Basyl helps you prepare a corporate growth strategy to help you grow and lead the competition.
 
                   At Basyl, we take a deeper look at your business operations. With our advanced tools and trained experts, we conduct corporate finance consulting in a way that brings unique data-backed strategies to life in surprising ways to form an efficient and effective business growth strategy. Using our experience and innovation, our clients transform from market supporters to market leaders.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { HashLink } from "react-router-hash-link";
 import Logo from "../resources/Basyl_logo.png";
+import { NavDropdown, NavDropdownItem } from 'react-bootstrap';
 
 function NavElement(props) {
   return (
@@ -15,7 +16,7 @@ function Navbar() {
       <div className="logo">
         <img alt="logo" className="logo-navbar" src={Logo} />
       </div>
-      <nav className="menu">
+      <div className="menu big">
         <ul className="menu-item">
           <NavElement to="#home" name="Home" />
           <NavElement to="#solutions" name="Solutions" />
@@ -23,7 +24,32 @@ function Navbar() {
           <NavElement to="#services" name="Services" />
           <NavElement to="#contacts" name="Contacts" />
         </ul>
-      </nav>
+      </div>
+
+      <NavDropdown title="Menu" id="nav-dropdown" className="small">
+
+        <NavDropdown.Item eventKey="4.1" className="navSmall">
+          <NavElement to="#home" name="Home" className="navsmall"/>
+        </NavDropdown.Item>
+
+        <NavDropdown.Item eventKey="4.1">
+          <NavElement to="#solutions" name="Solutions" />
+        </NavDropdown.Item>
+
+        <NavDropdown.Item eventKey="4.2">
+          <NavElement to="#aboutus" name="About Us" />
+        </NavDropdown.Item>
+
+        <NavDropdown.Item eventKey="4.3">
+          <NavElement to="#services" name="Services"/>
+        </NavDropdown.Item>
+        
+        <NavDropdown.Item eventKey="4.3">
+          <NavElement to="#contacts" name="Contacts" />
+        </NavDropdown.Item>
+
+      </NavDropdown>
+
     </header>
   );
 }
