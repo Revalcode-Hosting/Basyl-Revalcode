fdc995dd29a51472583c5ac376f5234836ad60f3
Sophiae <sophia.makita@revalcodesolution.comm>
2022-02-21 11:11:42 -0300

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,10 @@ import logo from "../resources/Basyl_logo_footer.png";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer text-center">
       <div>
-        <img src={logo} alt="logo" />
+        <img className="logoFooter" src={logo} alt="logo" />
+        <p>| Copyright © 2022 All rights reserved |</p>
       </div>
     </footer>
   );
